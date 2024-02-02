b35f22233b0cc312c51cb8b975afe1c3d6e32697
Mohammed Saad <maazsaad13@gmail.com>
2024-02-03 04:43:06 +0530

    Restrict web access

--- build.php
+++ build.php
@@ -4,6 +4,11 @@ require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/src/WebToApp.php';
 
+// Check if script is running from the command line.
+if (php_sapi_name() !== 'cli') {
+    die('This script can only be run from the command line.');
+}
+
 /**
  * Start the conversion process.
  * 
